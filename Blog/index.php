3665b6e09a27dae89299fcbac5246997ed0ff557
trenthalstad <trentonmhasltad@fvcc.edu>
2016-01-25 16:02:58 -0700

    Base Html site
    This is just something to throw up

---
+++ Blog/index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+                <!-- Bootstrap -->
+        <link rel="stylesheet" type="text/css"  href="css/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="fonts/font-awesome/css/font-awesome.css">
+
+        <!-- Stylesheet -->
+        <link rel="stylesheet" type="text/css"  href="css/styles.css">
+
+        <script type="text/javascript" src="js/modernizr.custom.js"></script>
+    </head>
+    <body>
+
+        <?php
+        include "menu.php";
+        ?>
+        <div class="container content">
+                            <div class="row">
+                                <div class="col-md-12">
+
+                                    <h2>Blog City<br>
+                                    <h3>The best blog cite around!</h3>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="container center">
+                            <div class="row">
+                                <div class="col-sm-12 col-md-6 col-lg-3">
+                                    <h3>Blogs!</h3>
+                                 
+                                    <img src=""  width=150px height=200px>
+								
+                                    
+
+                                </div>
+                                <div class="col-sm-12 col-md-6 col-lg-3">
+                                    <h3>Blogs</h3>
+                                 
+                                      <img src=""width=150px height=200px>
+
+                                </div>
+                                <div class="col-sm-12 col-md-6 col-lg-3">
+                                    <h3>Blogs</h3>
+                                   
+				<img src=""width=150px height=200px>
+
+                                </div>
+                                <div class="col-sm-12 col-md-6 col-lg-3">
+                                    <h3>Blogs</h3>
+                                   
+                                      <img src=""width=150px height=200px>
+
+                                </div>
+                            </div>
+                        </div>		
+
+
+      
+
+
+
+
+
+                        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+                        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+                        <script type="text/javascript" src="js/jquery.1.11.1.js"></script>
+                        <!-- Include all compiled plugins (below), or include individual files as needed -->
+                        <script type="text/javascript" src="js/bootstrap.js"></script>
+                        <script type="text/javascript" src="js/SmoothScroll.js"></script>
+                        <script type="text/javascript" src="js/jquery.isotope.js"></script>
+
+                        <script src="js/owl.carousel.js"></script>
+
+                        <!-- Javascripts
+                        ================================================== -->
+                        <script type="text/javascript" src="js/main.js"></script>
+    </body>
+</html>
---
+++ Blog/menu.php
@@ -0,0 +1,67 @@
+<!--     ///////////////////////////////////////////////////////  -->
+           <nav id="cs-menu" class="navbar navbar-default navbar-fixed-top">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.php">BLOG CITY</a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+                    <ul class="nav navbar-nav navbar-right">
+                        <li> <a href="index.php">About</a></li> 
+                             <li> <a href="blog1.php">Blog 1</a></li> 
+                                  <li> <a href="blog2.php">Blog 2</a></li> 
+                                       <li> <a href="blog3.php">Blog 3</a></li> 
+                                       <li> <a href="contact.php">Contact</a></li> 
+                        <!--
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Objectives <b class="caret"></b></a>
+                            <ul class="dropdown-menu">
+                                <li><a href ="unity.php">Unity</a></li>
+
+                            </ul>
+
+                        </li>                  
+
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Notes <b class="caret"></b></a>
+                            <ul class="dropdown-menu multi-level" role="menu" aria-labelledby="dropdownMenu">
+                                <li><a href ="bylaws.php">Bylaws Proposal</a></li>
+                                <li class="divider"></li>
+                                <li class="dropdown-submenu">
+                                    <a tabindex="-1" href="#">2015/2016 Notes</a>
+                                    <ul class="dropdown-menu">
+                                        <!--<li><a tabindex="-1" href="#">Second level</a></li>-->
+                          
+<!--
+                                <li><a href ="notes1.php">09/11/2015 Notes</a>
+                                <li><a href ="notes2.php">09/18/2015 Notes</a></li>
+                                <li><a href ="notes3.php">09/25/2015 Notes</a></li>
+                                <li><a href ="notes4.php">10/02/2015 Notes</a></li>
+                                <li><a href="notes5.php">10/09/2015 Notes</a></li>
+                                <li><a href="notes6.php">10/16/2015 Notes</a></li>
+								<li><a href="notes7.php">10/23/2015 Notes</a></li>
+-->
+                                                          
+                                        <!--<li><a href = "https://docs.google.com/document/d/1AyxWZMwJDy6TrEXyuGzYyLsKdQJ5DoMZY4qgu4SC9Xo/edit?usp=sharing">Minutes</a></li>-->
+
+                                    </ul>
+          
+                                </li>
+                          
+                        
+                                  </ul>
+                        </li>
+
+
+
+                        </nav>
+     
