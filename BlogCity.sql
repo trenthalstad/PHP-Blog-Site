f1ea1480c6fa8d0cedbdb5b517095e637f8d1fdd
Richardahull <richardahull@fvcc.edu>
2016-01-28 13:00:08 -0700

    Rick changes - Database
    Revised user table

--- BlogCity.sql
+++ BlogCity.sql
@@ -6,15 +6,17 @@ USE BlogCity_db;
 
 -- --------------------------------------------------------
 --
--- Table structure for table `blogarticles`
+-- Table structure for table  blog articles 
 --
 
 CREATE TABLE IF NOT EXISTS `articles_tbl` (
   `UserID` int(10) NOT NULL,
-  `Article` text NOT NULL,
-  `DateEntered` date NOT NULL,
-  `DateEdited` date NOT NULL,
-  `DateDeleted` date NOT NULL,
+  `Title` varchar(255),
+  `Article` text,
+  `DateAdded` date,
+  `DateEdited` date,
+  `DateDeleted` date,
+  `Deleted` boolean,
   `ArticleID` int(6) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY(ArticleID)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -22,16 +24,17 @@ CREATE TABLE IF NOT EXISTS `articles_tbl` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `userinfo`
+-- Table structure for table  user info 
 --
 CREATE TABLE IF NOT EXISTS `userinfo_tbl` (
-  `FirstName` varchar(255) NOT NULL,
-  `LastName` varchar(255) NOT NULL,
-  `Address` varchar(255) NOT NULL,
-  `City` varchar(50) NOT NULL,
-  `State` varchar(50) NOT NULL,
-  `Zip` varchar(25) NOT NULL,
-  `Email` varchar(255) NOT NULL,
+  `UserName` varchar(50) NOT NULL,
+  `FirstName` varchar(50),
+  `LastName` varchar(50),
+  `Address` varchar(255),
+  `City` varchar(50),
+  `State` varchar(50),
+  `Zip` varchar(25),
+  `Email` varchar(50),
   `Password` varchar(255) NOT NULL,
   `UserID` int(10) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY(UserID)
