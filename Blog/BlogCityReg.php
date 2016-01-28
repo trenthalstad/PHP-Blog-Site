361c14cd4a19321d21d3d345aec175a6c955638e
trenthalstad <trentonmhalstad@fvcc.edu>
2016-01-28 13:37:22 -0700

    Update BlogCityReg.php

--- Blog/BlogCityReg.php
+++ Blog/BlogCityReg.php
@@ -2,22 +2,13 @@
 require "Common.php";
 print_r($_POST);
 
-<<<<<<< HEAD
 if (isset($_POST['UserName'])) {
     $pwd = $_POST['Password'];
-=======
-if (isset($_POST['username'])) {
-    $pwd = $_POST['password'];
->>>>>>> origin/master
 
 
     
     //create sql statement
-<<<<<<< HEAD
     $sql_stmt = "INSERT INTO userinfo_tbl "
-=======
-    $sql_stmt = "INSERT INTO tbl_user "
->>>>>>> origin/master
             . "(UserName, "
             . "FirstName, "
             . "LastName, "
@@ -25,11 +16,7 @@ if (isset($_POST['username'])) {
             . "City, "
             . "State, "
             . "Zip, "
-<<<<<<< HEAD
             . "Email, "
-=======
-            . "Eamil, "
->>>>>>> origin/master
             . "Password) "
             . "VALUES "
             . "(:UserName, "
@@ -39,18 +26,13 @@ if (isset($_POST['username'])) {
             . ":City, "
             . ":State, "
             . ":Zip, "
-<<<<<<< HEAD
             . ":Email, "
-=======
-            . ":Eamil, "
->>>>>>> origin/master
             . ":Password)";
 
     //prepare the sql statement
     $sqlh = $pdo->prepare($sql_stmt);
 
     //sanitize the input
-<<<<<<< HEAD
     $in_username = filter_var($_POST['UserName'], FILTER_SANITIZE_STRING);
     $in_firstname = filter_var($_POST['FirstName'], FILTER_SANITIZE_STRING);
     $in_lastname = filter_var($_POST['LastName'], FILTER_SANITIZE_STRING);
@@ -59,40 +41,20 @@ if (isset($_POST['username'])) {
     $in_state = filter_var($_POST['State'], FILTER_SANITIZE_STRING);
     $in_zip = filter_var($_POST['Zip'], FILTER_SANITIZE_STRING);
     $in_email = filter_var($_POST['Email'], FILTER_SANITIZE_STRING);
-=======
-    $in_username = filter_var($_POST['Username'], FILTER_SANITIZE_STRING);
-    $in_username = filter_var($_POST['Firstname'], FILTER_SANITIZE_STRING);
-    $in_username = filter_var($_POST['Lastname'], FILTER_SANITIZE_STRING);
-    $in_username = filter_var($_POST['Address'], FILTER_SANITIZE_STRING);
-    $in_username = filter_var($_POST['City'], FILTER_SANITIZE_STRING);
-    $in_username = filter_var($_POST['State'], FILTER_SANITIZE_STRING);
-    $in_username = filter_var($_POST['Zip'], FILTER_SANITIZE_STRING);
-    $in_username = filter_var($_POST['Eamil'], FILTER_SANITIZE_STRING);
->>>>>>> origin/master
     $in_password = filter_var($_POST['Password'], FILTER_SANITIZE_STRING);
 
     //hash the password
     $in_password = password_hash($in_password, PASSWORD_DEFAULT);
 
     //bind the parameters
-<<<<<<< HEAD
     $sqlh->bindparam(":UserName", $in_username);
     $sqlh->bindparam(":FirstName", $in_firstname);
     $sqlh->bindparam(":LastName", $in_lastname);
-=======
-    $sqlh->bindparam(":Username", $in_username);
-    $sqlh->bindparam(":Firstname", $in_firstname);
-    $sqlh->bindparam(":Lastname", $in_lastname);
->>>>>>> origin/master
     $sqlh->bindparam(":Address", $in_address);
     $sqlh->bindparam(":City", $in_city);
     $sqlh->bindparam(":State", $in_state);
     $sqlh->bindparam(":Zip", $in_zip);
-<<<<<<< HEAD
     $sqlh->bindparam(":Email", $in_email);
-=======
-    $sqlh->bindparam(":Email", $in_eamil);
->>>>>>> origin/master
     $sqlh->bindparam(":Password", $in_password);
 
     //excecute the sqlstatement
@@ -108,11 +70,7 @@ if (isset($_POST['username'])) {
     
 
     echo "        <div id='newuser' >
-<<<<<<< HEAD
             <form method='POST' action='BlogCityReg.php'>
-=======
-            <form method='POST' action='NewUserRegistration.php'>
->>>>>>> origin/master
                 <table >
                     <tbody>
                         <tr>
